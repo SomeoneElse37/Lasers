ff510a4839f841346316cc77e604e2f539709017
Aaron Williams <amwill19@ncsu.edu>
2018-07-05 12:56:39 -0400

    Added docstring to timestamper.py

--- timestamper.py
+++ timestamper.py
@@ -1,3 +1,10 @@
+"""
+This script simply copies stdin to stdout line by line, with the exact time between when
+the script was started to when the line was received prepended to each line.
+
+I recommend running this script from the command line and using your shell to redirect the output to a file. Eg:
+python timestamper.py > log.txt
+"""
 
 import datetime
 
